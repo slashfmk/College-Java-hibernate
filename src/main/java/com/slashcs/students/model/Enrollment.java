93a1e48d3e13a4bef81780b06a901f8b0ea3b5f8
Yannick fumukani <slashcs7@hotmail.com>
2023-03-29 21:26:15 -0500

    Enrollment class created

---
+++ src/main/java/com/slashcs/students/model/Enrollment.java
@@ -0,0 +1,47 @@
+package com.slashcs.students.model;
+
+
+import com.slashcs.students.composite.EnrollmentId;
+import jakarta.persistence.*;
+
+@Entity(name = "enrollment")
+@Table(name = "enrollment")
+public class Enrollment {
+
+    @EmbeddedId
+    private EnrollmentId id;
+
+    @ManyToOne
+    @MapsId("studentId")
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @MapsId("courseId")
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    public Enrollment() {
+    }
+
+    public Enrollment(Student student, Course course) {
+        this.student = student;
+        this.course = course;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
